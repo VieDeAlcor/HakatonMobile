ff4144c9677d9125d81a4b816e923bbb7f094ac1
Alcor <Alcor@DESKTOP-8A8N62S>
2022-02-15 17:12:31 +0500

    Добавьте файлы проекта.

---
+++ App6/App6/MainPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class MainPage : ContentPage
+    {    
+        static User user;
+        public MainPage()
+        {
+                      
+            InitializeComponent();
+            
+        }
+
+        public void LoginCliked_Button(object sender, EventArgs e)
+        {
+            try
+            {
+                string login = log.Text;
+                string password = pass.Text;
+
+                using (var dbContext = new ApplicationContext())
+                {
+                    user = dbContext.Users.Where(user => (user.Email == login) && (user.Password == password)).First();
+                }
+                if (user.Pin == "" || user.Pin == null) { Application.Current.MainPage = new NewPinCode(user); }
+                else { Application.Current.MainPage = new EnterPinCode(user); }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ App6/App6/MenuPril.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Android;
+using System.IO;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Svg;
+using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+using ZXing.QrCode;
+
+namespace App6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        User users;
+        public Page1(User user)
+        {
+            users = user;
+            InitializeComponent();
+            zxingImageView.BarcodeValue = user.ID.ToString();
+            Position.Text = user.Position;
+            nameOfCorp.Text = user.NameOfCorp;
+            firstnameOfUser.Text = user.Surname;
+            nameOfUser.Text = user.Name;
+            puperOfUser.Text = user.Patronymic;
+            dateOfUser.Text = user.Date;
+        }
+
+        private void ClickedBack_Button(object sender, System.EventArgs e)
+        {
+            Application.Current.MainPage = new MainPage();
+        }
+
+        private void ClickedNewPin_Button(object sender, System.EventArgs e)
+        {
+            Application.Current.MainPage = new NewPinCode(users);
+        }
+    }
+}
